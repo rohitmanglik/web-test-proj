28565c72fb6c62d0420c9b0c2d7f452e971d169e
Rohit Manglik <manglik@deshaw.com>
2015-07-07 02:02:16 -0400

    Correct .idea ignore issue

--- python/MyFirstProgramInPython.py
+++ python/MyFirstProgramInPython.py
@@ -1,3 +1,13 @@
-votes = "y y n y n"
+#!/usr/local/bin/python
 
-upper_votes = votes.upper()
+from deshaw.optvol.memcache import shared_memoize
+
+
+@shared_memoize(k)
+def test_memoize_function(param=None):
+    print param
+    return param+"a"
+
+test_memoize_function("1")
+test_memoize_function("2")
+test_memoize_function("1")
